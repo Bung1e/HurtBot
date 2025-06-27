53f3e33380feb1298191ea3caa2e7ae17b42592b
Bung1e <leshkachovzhik@gmail.com>
2025-06-27 18:16:28 +0200

    feat: enhance evaluation framework with semantic similarity scoring and visualization
    - Added new evaluation scripts for generating model answers and evaluating semantic similarity.
    - Introduced histogram and boxplot visualizations for semantic similarity metrics.
    - Updated test question-answer dataset with refined expected answers.
    - Added new dependencies: prompttools, matplotlib, and sentence-transformers in pyproject.toml.

---
+++ eval/evaluate_answers.py
@@ -0,0 +1,32 @@
+import json
+from typing import Any, Dict
+
+import pandas as pd
+from prompttools.utils import semantic_similarity
+from tqdm import tqdm
+
+with open("eval/model_answers.json", encoding="utf-8") as f:
+    data: list[dict[str, Any]] = json.load(f)
+
+df: pd.DataFrame = pd.DataFrame(data)
+
+
+def eval_row(row: pd.Series) -> Dict[str, float]:
+    # autoeval = autoeval_scoring(
+    #     row, row["expected_answer"], response_column_name="model_answer"
+    #)
+    semsim: float = semantic_similarity(
+        row, row["expected_answer"], response_column_name="model_answer"
+    )
+    return {
+        # "autoeval_score": autoeval,
+        "semantic_similarity": semsim
+    }
+
+
+results: list[Dict[str, float]] = [eval_row(row) for _, row in tqdm(df.iterrows(), total=len(df))]
+
+results_df: pd.DataFrame = pd.DataFrame(results)
+df = pd.concat([df, results_df], axis=1)
+
+df.to_json("eval/result.json", orient="records", force_ascii=False, indent=2)
